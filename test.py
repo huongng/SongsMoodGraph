321e39fa194674ab9945900ed42f51f7e9c79cdf
Huong <huong.nguyen0395@gmail.com>
2018-01-14 20:32:46 -0500

    initial commit

---
+++ test.py
@@ -0,0 +1,6 @@
+import indicoio
+indicoio.config.api_key = '2787bf682fdd9d44353da7d57e72246a'
+
+# first test for emotion analysis
+print indicoio.emotion("As the cheerless towns pass my window. I can see a washed out moon through the fog")
+
